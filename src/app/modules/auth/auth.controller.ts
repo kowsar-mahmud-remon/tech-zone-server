b36eaf8e8229a27b8340667d1b84635efdb3cd10
creativerze <creativerze@gmail.com>
2023-10-13 01:07:15 +0600

    added login user access token

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import catchAsync from '../../../shared/catchAsync';
+import { generateToken } from '../../../shared/generateToken';
 import { AuthService } from './auth.service';
 
 const loginUser = catchAsync(async (req: Request, res: Response) => {
@@ -7,11 +8,18 @@ const loginUser = catchAsync(async (req: Request, res: Response) => {
 
   const result = await AuthService.loginUser(loginUserData);
 
+  const token = generateToken({
+    _id: loginUserData?._id,
+    email: loginUserData?.email,
+    role: loginUserData?.role,
+  });
+
   res.status(200).json({
     statusCode: 200,
     success: true,
     message: 'User logged in successfully !',
     data: result,
+    token: token,
   });
 });
 
