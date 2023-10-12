4ef0cc03b8e7030206c0ffce4233ec1d7095ef90
creativerze <creativerze@gmail.com>
2023-10-13 00:50:55 +0600

    update secret

--- src/shared/generateToken.ts
+++ src/shared/generateToken.ts
@@ -1,10 +1,11 @@
 import jwt from 'jsonwebtoken';
+import config from '../config';
 import { IUserAccessTokenData } from '../interfaces/common';
 
 export const generateToken = (
   data: IUserAccessTokenData,
   expiresIn = '180d'
 ) => {
-  const token = jwt.sign(data, process.env.JWT_SECRET as string, { expiresIn });
+  const token = jwt.sign(data, config.jwt.secret as string, { expiresIn });
   return token;
 };
