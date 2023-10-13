f40838a9ec096f9d656034da05c9a35cfc989f04
creativerze <creativerze@gmail.com>
2023-10-13 11:27:00 +0600

    added all user route

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -11,4 +11,33 @@ router.post(
   UserController.createUser
 );
 
+router.get(
+  '/:id',
+  // auth(
+  //   ENUM_USER_ROLE.SUPER_ADMIN,
+  //   ENUM_USER_ROLE.ADMIN,
+  // ),
+  UserController.getSingleUser
+);
+router.delete(
+  '/:id',
+  // auth(ENUM_USER_ROLE.SUPER_ADMIN),
+  UserController.deleteUser
+);
+
+router.patch(
+  '/:id',
+  // validateRequest(StudentValidaion.updateStudentZodSchema),
+  // auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  UserController.updateUser
+);
+router.get(
+  '/',
+  // auth(
+  //   ENUM_USER_ROLE.SUPER_ADMIN,
+  //   ENUM_USER_ROLE.ADMIN,
+  // ),
+  UserController.getAllUsers
+);
+
 export const UserRoutes = router;
